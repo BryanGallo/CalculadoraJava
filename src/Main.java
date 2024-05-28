9c0bb52f36a06885e150524361035246a1bf1e8f
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-28 01:50:59 -0500

    Solicitando valores e imprimiendo la suma

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Calculadora");
+        Scanner consola = new Scanner(System.in);
+
+        System.out.println("*** Aplicacion Calculadora ***");
+        System.out.println("Proporciona el primer valor:");
+        int valor1 = Integer.parseInt(consola.nextLine());
+        System.out.println("Proporciona el segundo valor:");
+        int valor2 = Integer.parseInt(consola.nextLine());
+        int result = valor1 + valor2;
+
+        System.out.print("El resultado es: ");
+        System.out.println(result);
     }
 }
