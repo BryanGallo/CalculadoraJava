76d211ec1f966cd9d3413566d166d3609225e77a
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-29 23:50:24 -0500

    Refactorizando el codigo creando funciones

--- src/Main.java
+++ src/Main.java
@@ -6,52 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
         int operation=0;
-        int result;
         do {
-            System.out.println("*** Aplicacion Calculadora ***");
-            //Menu
-            System.out.println("*** Que operacion deseas hacer ***");
-            System.out.println("""
-                1.Suma
-                2.Resta
-                3.Multiplicacion
-                4.Division
-                5.Salir
-                """);
-            System.out.println("Que opcion elijes?");
+            viewMenu();
             try {
                 operation = Integer.parseInt(consola.nextLine());
                 if (operation == 5){
                     System.out.println("Gracias por usar la calculadora Adios");
-
                 }else if( operation < 0 || operation > 5){
                     System.out.println("Opcion equivocada");
                 }else{
-                    System.out.println("Proporciona el primer valor:");
-                    int valor1 = Integer.parseInt(consola.nextLine());
-                    System.out.println("Proporciona el segundo valor:");
-                    int valor2 = Integer.parseInt(consola.nextLine());
-                    switch (operation) {
-                        case 1:
-                            result = valor1 + valor2;
-                            System.out.println("El resultado de la suma es: "+result);
-                            break;
-                        case 2:
-                            result = valor1 - valor2;
-                            System.out.println("El resultado de la resta es: "+result);
-                            break;
-                        case 3:
-                            result = valor1 * valor2;
-                            System.out.println("El resultado de la multiplicacion es: "+result);
-                            break;
-                        case 4:
-                            result = valor1 / valor2;
-                            System.out.println("El resultado de la division es: "+result);
-                            break;
-                        case 5:
-                            System.out.println("Gracias por usar la calculadora Adios");
-                            break;
-                    }
+                    doOpertarions(operation,consola);
                 }
             }catch (Exception e){
                 System.out.println("Ocurrio un error" + e.getMessage());
@@ -59,4 +23,47 @@ public class Main {
         }while(operation != 5);
 
     }
+    private static void viewMenu(){
+        System.out.println("*** Aplicacion Calculadora ***");
+        //Menu
+        System.out.println("*** Que operacion deseas hacer ***");
+        System.out.println("""
+                1.Suma
+                2.Resta
+                3.Multiplicacion
+                4.Division
+                5.Salir
+                """);
+        System.out.println("Que opcion elijes?");
+    }
+
+    private static void doOpertarions(int operation ,Scanner consola){
+        System.out.println("Proporciona el primer valor:");
+        int valor1 = Integer.parseInt(consola.nextLine());
+        System.out.println("Proporciona el segundo valor:");
+        int valor2 = Integer.parseInt(consola.nextLine());
+        int result;
+        switch (operation) {
+            case 1:
+                result = valor1 + valor2;
+                System.out.println("El resultado de la suma es: "+result);
+                break;
+            case 2:
+                result = valor1 - valor2;
+                System.out.println("El resultado de la resta es: "+result);
+                break;
+            case 3:
+                result = valor1 * valor2;
+                System.out.println("El resultado de la multiplicacion es: "+result);
+                break;
+            case 4:
+                result = valor1 / valor2;
+                System.out.println("El resultado de la division es: "+result);
+                break;
+            case 5:
+                System.out.println("Gracias por usar la calculadora Adios");
+                break;
+        }
+    }
 }
+
