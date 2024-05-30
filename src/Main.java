ee377f42a976bda3032d6a6eac916165f0751b38
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-29 23:06:50 -0500

    Creando menu usando doWhile y switch

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
+        int operation;
+        do {
+            System.out.println("*** Aplicacion Calculadora ***");
+            System.out.println("Proporciona el primer valor:");
+            int valor1 = Integer.parseInt(consola.nextLine());
+            System.out.println("Proporciona el segundo valor:");
+            int valor2 = Integer.parseInt(consola.nextLine());
+            //Menu
+            System.out.println("*** Que operacion deseas hacer ***");
+            System.out.println("""
+                1.Suma
+                2.Resta
+                3.Multiplicacion
+                4.Division
+                5.Salir
+                """);
+            System.out.println("Que opcion elijes?");
+            operation = Integer.parseInt(consola.nextLine());
+            switch (operation) {
+                case 1:
+                    System.out.println("SUMA");
+                    break;
+                case 2:
+                    System.out.println("RESTA");
+                    break;
+                case 3:
+                    System.out.println("MULTIPLICACION");
+                    break;
+                case 4:
+                    System.out.println("DIVISION");
+                    break;
+                case 5:
+                    System.out.println("Gracias por usar la calculadora Adios");
+                    break;
+                default:
+                    System.out.println("Opcion equivocada");
+            }
+        }while(operation != 5);
 
-        System.out.println("*** Aplicacion Calculadora ***");
-        System.out.println("Proporciona el primer valor:");
-        int valor1 = Integer.parseInt(consola.nextLine());
-        System.out.println("Proporciona el segundo valor:");
-        int valor2 = Integer.parseInt(consola.nextLine());
-        int result = valor1 + valor2;
-
-        System.out.print("El resultado es: ");
-        System.out.println(result);
     }
 }
