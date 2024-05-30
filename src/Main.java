fdb4b2257052c8de20f8cc481ba03a0070b80820
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-29 23:25:51 -0500

    Colocando sentencias if para validar operando

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,6 @@ public class Main {
         int result;
         do {
             System.out.println("*** Aplicacion Calculadora ***");
-            System.out.println("Proporciona el primer valor:");
-            int valor1 = Integer.parseInt(consola.nextLine());
-            System.out.println("Proporciona el segundo valor:");
-            int valor2 = Integer.parseInt(consola.nextLine());
             //Menu
             System.out.println("*** Que operacion deseas hacer ***");
             System.out.println("""
@@ -23,30 +19,42 @@ public class Main {
                 5.Salir
                 """);
             System.out.println("Que opcion elijes?");
+
             operation = Integer.parseInt(consola.nextLine());
-            switch (operation) {
-                case 1:
-                    result = valor1 + valor2;
-                    System.out.println("El resultado de la suma es: "+result);
-                    break;
-                case 2:
-                    result = valor1 - valor2;
-                    System.out.println("El resultado de la resta es: "+result);
-                    break;
-                case 3:
-                    result = valor1 * valor2;
-                    System.out.println("El resultado de la multiplicacion es: "+result);
-                    break;
-                case 4:
-                    result = valor1 / valor2;
-                    System.out.println("El resultado de la division es: "+result);
-                    break;
-                case 5:
-                    System.out.println("Gracias por usar la calculadora Adios");
-                    break;
-                default:
-                    System.out.println("Opcion equivocada");
+            if (operation == 5){
+                System.out.println("Gracias por usar la calculadora Adios");
+
+            }else if( operation > 5){
+                System.out.println("Opcion equivocada");
+            }else{
+                System.out.println("Proporciona el primer valor:");
+                int valor1 = Integer.parseInt(consola.nextLine());
+                System.out.println("Proporciona el segundo valor:");
+                int valor2 = Integer.parseInt(consola.nextLine());
+                switch (operation) {
+                    case 1:
+                        result = valor1 + valor2;
+                        System.out.println("El resultado de la suma es: "+result);
+                        break;
+                    case 2:
+                        result = valor1 - valor2;
+                        System.out.println("El resultado de la resta es: "+result);
+                        break;
+                    case 3:
+                        result = valor1 * valor2;
+                        System.out.println("El resultado de la multiplicacion es: "+result);
+                        break;
+                    case 4:
+                        result = valor1 / valor2;
+                        System.out.println("El resultado de la division es: "+result);
+                        break;
+                    case 5:
+                        System.out.println("Gracias por usar la calculadora Adios");
+                        break;
+
+                }
             }
+
         }while(operation != 5);
 
     }
