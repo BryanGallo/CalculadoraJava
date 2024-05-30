e2427c8816483df92265949565b7734d405b762d
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-29 23:34:20 -0500

    Agregando try catch

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
-        int operation;
+        int operation=0;
         int result;
         do {
             System.out.println("*** Aplicacion Calculadora ***");
@@ -19,42 +19,45 @@ public class Main {
                 5.Salir
                 """);
             System.out.println("Que opcion elijes?");
+            try {
+                operation = Integer.parseInt(consola.nextLine());
+                if (operation == 5){
+                    System.out.println("Gracias por usar la calculadora Adios");
 
-            operation = Integer.parseInt(consola.nextLine());
-            if (operation == 5){
-                System.out.println("Gracias por usar la calculadora Adios");
-
-            }else if( operation > 5){
-                System.out.println("Opcion equivocada");
-            }else{
-                System.out.println("Proporciona el primer valor:");
-                int valor1 = Integer.parseInt(consola.nextLine());
-                System.out.println("Proporciona el segundo valor:");
-                int valor2 = Integer.parseInt(consola.nextLine());
-                switch (operation) {
-                    case 1:
-                        result = valor1 + valor2;
-                        System.out.println("El resultado de la suma es: "+result);
-                        break;
-                    case 2:
-                        result = valor1 - valor2;
-                        System.out.println("El resultado de la resta es: "+result);
-                        break;
-                    case 3:
-                        result = valor1 * valor2;
-                        System.out.println("El resultado de la multiplicacion es: "+result);
-                        break;
-                    case 4:
-                        result = valor1 / valor2;
-                        System.out.println("El resultado de la division es: "+result);
-                        break;
-                    case 5:
-                        System.out.println("Gracias por usar la calculadora Adios");
-                        break;
-
+                }else if( operation > 5){
+                    System.out.println("Opcion equivocada");
+                }else{
+                    System.out.println("Proporciona el primer valor:");
+                    int valor1 = Integer.parseInt(consola.nextLine());
+                    System.out.println("Proporciona el segundo valor:");
+                    int valor2 = Integer.parseInt(consola.nextLine());
+                    switch (operation) {
+                        case 1:
+                            result = valor1 + valor2;
+                            System.out.println("El resultado de la suma es: "+result);
+                            break;
+                        case 2:
+                            result = valor1 - valor2;
+                            System.out.println("El resultado de la resta es: "+result);
+                            break;
+                        case 3:
+                            result = valor1 * valor2;
+                            System.out.println("El resultado de la multiplicacion es: "+result);
+                            break;
+                        case 4:
+                            result = valor1 / valor2;
+                            System.out.println("El resultado de la division es: "+result);
+                            break;
+                        case 5:
+                            System.out.println("Gracias por usar la calculadora Adios");
+                            break;
+                        default:
+                            System.out.println("Opcion equivocada");
+                    }
                 }
+            }catch (Exception e){
+                System.out.println("Ocurrio un error" + e.getMessage());
             }
-
         }while(operation != 5);
 
     }
